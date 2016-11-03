16fe9c49c9d206887ced06ecbb263013863fbd28
DVerweij <danyllo.verweij@student.uva.nl>
2016-11-04 00:42:23 +0100

    -Crash fixed -All parts besides body defaulted to invisible (only at start-up) -Eyes and nose checkBox linked to drawables

--- app/src/main/java/com/example/danyllo/danylloverweij_pset1/MrPotatohead.java
+++ app/src/main/java/com/example/danyllo/danylloverweij_pset1/MrPotatohead.java
@@ -1,5 +1,6 @@
 package com.example.danyllo.danylloverweij_pset1;
 
+import android.media.Image;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,15 +15,61 @@ import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MrPotatohead extends AppCompatActivity {
 
-    public ImageView eyes = (ImageView) findViewById(R.id.imageView6);
+    public static ImageView eyes;
+    public static ImageView ears;
+    public static ImageView nose;
+    public static ImageView glasses;
+    public static ImageView shoes;
+    public static ImageView eyebrows;
+    public static ImageView mustache;
+    public static ImageView arms;
+    public static ImageView hat;
+    public static ImageView mouth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mr_potatohead);
+        Boolean isFirstRun = getSharedPreferences("PREFERENCE", MODE_PRIVATE).getBoolean("isFirstRun", true);
+        if(isFirstRun){
+            startVisibilities();
+        }
     }
+    protected void startVisibilities() {
+        //eyes:
+        eyes = (ImageView) findViewById(R.id.imageView6);
+        eyes.setVisibility(View.INVISIBLE);
+        //ears:
+        ears = (ImageView) findViewById(R.id.imageView3);
+        ears.setVisibility(View.INVISIBLE);
+        //nose:
+        nose = (ImageView) findViewById(R.id.imageView7);
+        nose.setVisibility(View.INVISIBLE);
+        //glasses:
+        glasses = (ImageView) findViewById(R.id.imageView9);
+        glasses.setVisibility(View.INVISIBLE);
+        //shoes:
+        shoes = (ImageView) findViewById(R.id.imageView5);
+        shoes.setVisibility(View.INVISIBLE);
+        //eyebrows:
+        eyebrows = (ImageView) findViewById(R.id.imageView12);
+        eyebrows.setVisibility(View.INVISIBLE);
+        //mustache:
+        mustache = (ImageView) findViewById(R.id.imageView8);
+        mustache.setVisibility(View.INVISIBLE);
+        //hat:
+        hat = (ImageView) findViewById(R.id.imageView11);
+        hat.setVisibility(View.INVISIBLE);
+        //arms:
+        arms = (ImageView) findViewById(R.id.imageView2);
+        arms.setVisibility(View.INVISIBLE);
+        //mouth:
+        mouth = (ImageView) findViewById(R.id.imageView10);
+        mouth.setVisibility(View.INVISIBLE);
 
-    public void switchVisibility(View view) {
+    }
+    public void changeVisibility(View view) {
         boolean check = ((CheckBox) view).isChecked();
 
         switch (view.getId()) {
@@ -32,6 +79,14 @@ public class MrPotatohead extends AppCompatActivity {
                 } else {
                     eyes.setVisibility(View.INVISIBLE);
                 }
+                break;
+            case R.id.nose:
+                if (check) {
+                    nose.setVisibility(View.VISIBLE);
+                } else {
+                    nose.setVisibility(View.INVISIBLE);
+                }
+                break;
         }
     }
 }
